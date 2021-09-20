211143cb6281412424f5c753e21ddda9aff7a380
komenkx1 <komangwahyu@student.unud.ac.id>
2021-09-20 18:01:57 +0800

    first-commit

---
+++ app/src/main/java/com/example/appnote/FileHelper.java
@@ -0,0 +1,55 @@
+package com.example.appnote;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class FileHelper {
+
+    static void writeToFile(String filename, String data, MainActivity context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(filename, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "Penulisan File Gagal: " + e.toString());
+        }
+    }
+
+    static String readFromFile(MainActivity context, String filename) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(filename);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File Tidak Ditemukan: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Tidak Dapat Membaca File: " + e.toString());
+        }
+
+        return ret;
+    }
+}
